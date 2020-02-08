fe75a85f4ffb5a6e96250470fbf1c442269717f5
fpalting <60789423+fpalting@users.noreply.github.com>
2020-02-08 15:46:07 -0600

    Add files via upload

--- soft2project/src/soft2project/calculator.java
+++ soft2project/src/soft2project/calculator.java
@@ -15,19 +15,45 @@ import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import com.jgoodies.forms.layout.RowSpec;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Dimension;
 
 public class calculator {
 
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-	private JButton btnNewButton_2;
-	private JButton btnNewButton_3;
-	private JButton btnNewButton_4;
-	private JButton btnNewButton_5;
-	private JTextField textField_2;
+	private JFrame frmCalculator;
+	private JTextField txtInput1;
+	private JTextField txtInput2;
+	private JButton btnPow;
+	private JButton btnAdd;
+	private JButton btnMinus;
+	private JButton btnBin2Dec;
+	private JButton btnMultiply;
+	private JButton btnPowN;
+	private JButton btnSine;
+	private JButton btnCosine;
+	private JButton btnDec2Bin;
+	private JButton btnExponent;
+	private JButton btnTangent;
+	private JButton btnHex2Bin;
+	private JButton btnBin2Hex;
+	private JButton btnDec2Hex;
+	private JButton btnHex2Dec;
+	private JButton btnAbs;
+	private JButton btnLoge;
+	private JButton btnHexAdd;
+	private JButton btnHexSub;
+	private JTextField txtResult;
+	private JButton btnNroot;
+	private JButton btnLog10;
+	private JButton btn2Root;
+	private JButton btnBinAdd;
+	private JButton btnDivide;
+	private JButton btnBinSub;
 
 	/**
 	 * Launch the application.
@@ -37,7 +63,7 @@ public class calculator {
 			public void run() {
 				try {
 					calculator window = new calculator();
-					window.frame.setVisible(true);
+					window.frmCalculator.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -56,40 +82,149 @@ public class calculator {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new MigLayout("", "[97px][97px][5px][97px,grow][97px]", "[22px][25px][25px]"));
+		frmCalculator = new JFrame();
+		frmCalculator.setMinimumSize(new Dimension(825, 390));
+		frmCalculator.setBackground(Color.WHITE);
+		frmCalculator.setForeground(Color.WHITE);
+		frmCalculator.setTitle("Calculator");
+		frmCalculator.setBounds(100, 100, 825, 390);
+		frmCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCalculator.getContentPane().setLayout(new MigLayout("", "[97px,grow,fill][97px,grow,fill][97.00px,grow,fill][97px,grow,fill][grow,fill]", "[22px,grow,fill][25px,grow,fill][25px,grow,fill][25px,grow,fill][grow,fill][grow,fill]"));
 		
-		textField = new JTextField();
-		frame.getContentPane().add(textField, "cell 0 0,alignx right,aligny top");
-		textField.setColumns(10);
+		txtInput1 = new JTextField();
+		txtInput1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(txtInput1, "cell 0 0,grow");
+		txtInput1.setColumns(10);
 		
-		textField_1 = new JTextField();
-		frame.getContentPane().add(textField_1, "cell 1 0,alignx left,aligny top");
-		textField_1.setColumns(10);
+		txtInput2 = new JTextField();
+		txtInput2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(txtInput2, "cell 1 0,grow");
+		txtInput2.setColumns(10);
 		
-		textField_2 = new JTextField();
-		frame.getContentPane().add(textField_2, "cell 3 0 2 1,growx");
-		textField_2.setColumns(10);
+		txtResult = new JTextField();
+		txtResult.setBackground(new Color(204, 255, 255));
+		txtResult.setDisabledTextColor(Color.GREEN);
+		txtResult.setEditable(false);
+		txtResult.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(txtResult, "cell 2 0 3 1,grow");
+		txtResult.setColumns(10);
 		
-		btnNewButton_1 = new JButton("New button");
-		frame.getContentPane().add(btnNewButton_1, "cell 0 1,alignx left,aligny top");
+		btnAdd = new JButton("+");
+		btnAdd.setFont(new Font("Tahoma", Font.BOLD, 20));
+		frmCalculator.getContentPane().add(btnAdd, "cell 0 1,grow");
 		
-		btnNewButton_2 = new JButton("New button");
-		frame.getContentPane().add(btnNewButton_2, "cell 1 1,alignx left,aligny top");
+		btnMinus = new JButton("-");
+		btnMinus.setFont(new Font("Tahoma", Font.BOLD, 20));
+		frmCalculator.getContentPane().add(btnMinus, "cell 1 1,grow");
 		
-		btnNewButton_3 = new JButton("New button");
-		frame.getContentPane().add(btnNewButton_3, "cell 3 1,alignx left,aligny top");
+		btnMultiply = new JButton("\u00D7");
+		btnMultiply.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnMultiply.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frmCalculator.getContentPane().add(btnMultiply, "flowx,cell 2 1,grow");
 		
-		btnNewButton = new JButton("New button");
-		frame.getContentPane().add(btnNewButton, "cell 0 2,alignx left,aligny top");
+		btnDivide = new JButton("\u00F7");
+		btnDivide.setFont(new Font("Tahoma", Font.BOLD, 20));
+		frmCalculator.getContentPane().add(btnDivide, "cell 3 1,grow");
 		
-		btnNewButton_5 = new JButton("New button");
-		frame.getContentPane().add(btnNewButton_5, "cell 1 2,alignx left,aligny top");
+		btnAbs = new JButton("|X|");
+		btnAbs.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnAbs.setAlignmentY(Component.TOP_ALIGNMENT);
+		frmCalculator.getContentPane().add(btnAbs, "cell 4 1,grow");
 		
-		btnNewButton_4 = new JButton("New button");
-		frame.getContentPane().add(btnNewButton_4, "cell 3 2,alignx left,aligny top");
+		btnPow = new JButton("X\u00B2");
+		btnPow.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnPow, "cell 0 2,grow");
+		
+		btnPowN = new JButton("X\u207F");
+		btnPowN.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnPowN, "cell 1 2,grow");
+		
+		btnExponent = new JButton("10\u207F");
+		btnExponent.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnExponent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frmCalculator.getContentPane().add(btnExponent, "flowx,cell 2 2,grow");
+		
+		btnLog10 = new JButton("log(X)");
+		btnLog10.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnLog10, "cell 3 2,grow");
+		
+		btnLoge = new JButton("ln(X)");
+		btnLoge.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnLoge, "cell 4 2,grow");
+		
+		btnSine = new JButton("sin(X)");
+		btnSine.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnSine, "cell 0 3,grow");
+		
+		btnCosine = new JButton("cos(X)");
+		btnCosine.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnCosine, "cell 1 3,grow");
+		
+		btnTangent = new JButton("tan(X)");
+		btnTangent.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnTangent, "flowx,cell 2 3,grow");
+		
+		btn2Root = new JButton("\u221AX");
+		btn2Root.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btn2Root, "cell 3 3,grow");
+		
+		btnNroot = new JButton("\u207F\u221AX");
+		btnNroot.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnNroot, "flowx,cell 4 3,grow");
+		
+		btnBin2Dec = new JButton("Bin2Dec");
+		btnBin2Dec.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnBin2Dec, "cell 0 4,grow");
+		
+		btnDec2Bin = new JButton("Dec2Bin");
+		btnDec2Bin.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnDec2Bin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frmCalculator.getContentPane().add(btnDec2Bin, "cell 1 4,grow");
+		
+		btnHex2Bin = new JButton("Hex2Bin");
+		btnHex2Bin.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnHex2Bin, "flowx,cell 2 4,grow");
+		
+		btnBinAdd = new JButton("Bin Addition");
+		btnBinAdd.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnBinAdd, "cell 3 4,grow");
+		
+		btnHexAdd = new JButton("Hex Addition");
+		btnHexAdd.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnHexAdd, "cell 4 4,grow");
+		
+		btnBin2Hex = new JButton("Bin2Hex");
+		btnBin2Hex.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnBin2Hex, "cell 0 5,grow");
+		
+		btnDec2Hex = new JButton("Dec2Hex");
+		btnDec2Hex.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnDec2Hex.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frmCalculator.getContentPane().add(btnDec2Hex, "cell 1 5,grow");
+		
+		btnHex2Dec = new JButton("Hex2Dec");
+		btnHex2Dec.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnHex2Dec, "flowx,cell 2 5,grow");
+		
+		btnBinSub = new JButton("Bin Subtraction");
+		btnBinSub.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnBinSub, "cell 3 5");
+		
+		btnHexSub = new JButton("Hex Subtraction");
+		btnHexSub.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		frmCalculator.getContentPane().add(btnHexSub, "cell 4 5,grow");
 	}
 
 }
