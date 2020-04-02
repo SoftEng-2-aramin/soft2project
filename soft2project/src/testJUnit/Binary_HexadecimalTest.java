0e470855ac680a188a6be2146a81e56b73b4582a
Vu Pham <wuenwu272@gmail.com>
2020-04-02 12:12:46 -0500

    100% test case and implement equivalence partitioning

--- soft2project/src/testJUnit/Binary_HexadecimalTest.java
+++ soft2project/src/testJUnit/Binary_HexadecimalTest.java
@@ -9,82 +9,143 @@ import soft2project.Binary_Hexadecimal;
 class Binary_HexadecimalTest {
 
 	@Test
-	 public void testBinAdd() {
+	 public void testValidBinAdd() {
+		double test1 = Double.parseDouble(Binary_Hexadecimal.BinAdd(1, 1)); 
+		double answer1 = 10;
+		assertEquals(test1, answer1);
+	}
+	
+	@Test
+	 public void testInvalidBinAdd() {
 		double test1 = Double.parseDouble(Binary_Hexadecimal.BinAdd(1, 2)); 
-		double answer1 = 11;
+		double answer1 = 10;
+		assertNotEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testValidBinSub() {
+		double test1 = Double.parseDouble(Binary_Hexadecimal.BinSub(1, 1)); 
+		double answer1 = 0;
 		assertEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testBinSub() {
-		double test1 = Double.parseDouble(Binary_Hexadecimal.BinSub(9, 1)); 
+	public void testInvalidBinSub() {
+		double test1 = Double.parseDouble(Binary_Hexadecimal.BinSub(1, 2)); 
 		double answer1 = 1000;
-		System.out.println(Binary_Hexadecimal.BinSub(9, 1));
-		assertEquals(test1, answer1);
+		assertNotEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testHexAdd() {
+	public void testValidHexAdd() {
 		String test1 = Binary_Hexadecimal.HexAdd(10, 1); 
-		String answer1 = "b";
-		System.out.println(Binary_Hexadecimal.HexAdd(10, 1)); 
+		String answer1 = "b"; 
 		assertEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testHexSub() {
+	public void testInvalidHexAdd() {
+		String test1 = Binary_Hexadecimal.HexAdd(17, 1); 
+		String answer1 = "g"; 
+		assertNotEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testValidHexSub() {
 		String test1 = Binary_Hexadecimal.HexSub(15, 1); 
 		String answer1 = "e";
-		System.out.println(Binary_Hexadecimal.HexSub(15, 1)); 
 		assertEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testBin2Dec() {
-		String test1 = Binary_Hexadecimal.Bin2Dec(9); 
-		String answer1 = "9";
-		System.out.println(Binary_Hexadecimal.Bin2Dec(9)); 
+	public void testInvalidHexSub() {
+		String test1 = Binary_Hexadecimal.HexSub(17, 1); 
+		String answer1 = "g";
+		assertNotEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testValidBin2Dec() {
+		String test1 = Binary_Hexadecimal.Bin2Dec(1); 
+		String answer1 = "1"; 
 		assertEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testDec2Bin () {
+	public void testInvalidBin2Dec() {
+		String test1 = Binary_Hexadecimal.Bin2Dec(2); 
+		String answer1 = "10"; 
+		assertNotEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testValidDec2Bin () {
 		String test1 = Binary_Hexadecimal.Dec2Bin(9); 
-		String answer1 = "1001";
-		System.out.println(Binary_Hexadecimal.Dec2Bin(9)); 
+		String answer1 = "1001"; 
 		assertEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testHex2Dec() {
+	public void testInvalidDec2Bin () {
+		String test1 = Binary_Hexadecimal.Dec2Bin(10); 
+		String answer1 = "1001"; 
+		assertNotEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testValidHex2Dec() {
 		String test1 = Binary_Hexadecimal.Hex2Dec(11); 
 		String answer1 = "11";
-		System.out.println(Binary_Hexadecimal.Hex2Dec(11)); 
 		assertEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testDec2Hex() {
+	public void testInvalidHex2Dec() {
+		String test1 = Binary_Hexadecimal.Hex2Dec(17); 
+		String answer1 = "16";
+		assertNotEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testValidDec2Hex() {
 		String test1 = Binary_Hexadecimal.Dec2Hex(15); 
 		String answer1 = "f";
-		System.out.println(Binary_Hexadecimal.Dec2Bin(15)); 
 		assertEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testBin2Hex() {
+	public void testInvalidDec2Hex() {
+		String test1 = Binary_Hexadecimal.Dec2Hex(10); 
+		String answer1 = "10";
+		assertNotEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testValidBin2Hex() {
+		String test1 = Binary_Hexadecimal.Bin2Hex(1); 
+		String answer1 = "1";
+		assertEquals(test1, answer1);
+	}
+	
+	@Test
+	public void testInvalidBin2Hex() {
 		String test1 = Binary_Hexadecimal.Bin2Hex(15); 
-		String answer1 = "f";
-		System.out.println(Binary_Hexadecimal.Bin2Hex(15)); 
-		assertEquals(test1, answer1);
+		String answer1 = "15";
+		assertNotEquals(test1, answer1);
 	}
 	
 	@Test
-	public void testHex2Bin() {
+	public void testValidHex2Bin() {
 		String test1 = Binary_Hexadecimal.Hex2Bin(9); 
 		String answer1 = "1001";
-		System.out.println(Binary_Hexadecimal.Hex2Bin(9)); 
 		assertEquals(test1, answer1);	
+	}
+	
+	@Test
+	public void testInvalidHex2Bin() {
+		String test1 = Binary_Hexadecimal.Hex2Bin(17); 
+		String answer1 = "10010";
+		assertNotEquals(test1, answer1);	
 	}
 
 }
